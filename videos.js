f06bfb55cac935b874347244e04403fb26d5f7b8
Bwxrm <bryangreen921@gmail.com>
2025-11-29 21:32:08 -0500

    Add list of compressed videos to videos.js

---
+++ videos.js
@@ -0,0 +1,27 @@
+// List of compressed videos stored in /videos
+
+const videos = [
+  "10031B32e8b24e50b457e89a153ad6cc.mp4",
+  "1530ce1ae24a48879359ed82386d5e8.mp4",
+  "1A81dd32968742e19a0e32c49a162840(1).mp4",
+  "2Db24d39fe9b43adb4ed6390bf445d8b.mp4",
+  "3985C091d8974757aca5dc7b31f9ce2b.mp4",
+  "55068Dbfdf7e42419714660fb54d2b59.mp4",
+  "572194C589614b23bf2f1d9d66e75095.mp4",
+  "586F08cea03e48bcb4d64732fe42a3b3.mp4",
+  "78449456555_5553E778-B751-4452-A562-1DD2719B2581.mp4",
+  "78Fafd3ba5fa41ee923a74efd50cdc0b.mp4",
+  "B000a1541de345d0820c42264c6a5420.mp4",
+  "B7cc5e924cd64a769fe68b06983cd04c.mp4",
+  "C04a934a51cd428ab029d06837937136.mp4",
+  "Copy 74Af4d4d-E053-40D2-99D9-856826F4f52e.mp4",
+  "Copy B96533C7-A732-421C-8E7A-512BEAB0349A.mp4",
+  "E59c1b0ea6a74e7f8eb344f7bddadb24.mp4",
+  "E5c7fff4853d40faa80a0c0988df9c8e.mp4",
+  "Img 0097.mp4",
+  "Img 0176.mp4",
+  "Img 0207(1).mp4",
+  "Img 0250.mp4",
+  "RenderedVideo.mp4",
+  "V12044gd0000d0CGPl7og65l5q3gg4kg.mp4"
+];
